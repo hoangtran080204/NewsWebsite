b41a2726c01812c3d966800e4fd65c4970362610
Hoang Tran <tranvietnguyenhoang2004@gmail.com>
2024-05-21 22:36:59 -0700

    Add logging and docstring for search function

--- server/server.py
+++ server/server.py
@@ -1,3 +1,4 @@
+import logging
 from flask import Flask, request, jsonify
 from newsapi import NewsApiClient
 
@@ -13,9 +14,14 @@ app = Flask(__name__)
 #Init News Api
 newsapi = NewsApiClient(api_key=os.getenv("NEWS_API_KEY"))
 
-#API endpoint for handling search request based on user input
+#Init logger
+logger = logging.getLogger(__name__)
+
 @app.route("/search", methods = ["GET"])
 def search():
+    """
+    API endpoint for handling search request based on user input
+    """
     if request.method == "GET":
         searched_term = request.args.get("q")
 
@@ -25,14 +31,15 @@ def search():
             if response['status'] == 'ok':
                 searched_articles = response['articles']
                 return jsonify(searched_articles)
-            elif response['status'] == 'error':
-                # Return specific error message from NewsAPI in case of failure
-                error_message = response['message']
-                return jsonify({"error": error_message}), 500
+            else:
+                # Logging specific error from NewsAPI in case of failure
+                logger.warning(f"newsapi.get_everything has error: {response}")
+                return jsonify({"error": "API Request Failed."}), 500
 
         except Exception as e:
             # Handle any exceptions that are not explicitly raised by NewsAPI
-            return jsonify({"error": str(e)}), 500
-
+            logger.exception(f"Exception errors when calling newsapi.get_everything: {e}")
+            return jsonify({"error": "API Request Failed."}), 500
+        
 if __name__ == "__main__":
     app.run(debug=True)
