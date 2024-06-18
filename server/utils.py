e3656ede095a20409477787bfde9629d9aa70d3d
Kevin <68169009+hoangtran080204@users.noreply.github.com>
2024-06-18 00:11:48 -0700

    feat: Refactor search feature by implementing function for querying matching articles from the database (#4)
    * feat: create helper function to format Article object
    
    * feat: create query function to retrieve article from database; refactor search function
    
    * feat: add helper method to include metadata for api response

--- server/utils.py
+++ server/utils.py
@@ -15,3 +15,52 @@ def celery_init_app(app: Flask) -> Celery:
     celery_app.set_default()
     app.extensions["celery"] = celery_app
     return celery_app
+
+def article_to_dict(article):
+    """
+    Convert an Article object to a matching format similar to the response object from NewsAPI.
+
+    :param article: Article object
+    :return: Dictionary representation of the article object
+    """
+    return {
+        "source": {
+            "name": article.source_name
+        },
+        "author": article.author,
+        "title": article.title,
+        "description": article.description,
+        "url": article.url,
+        "urlToImage": article.image_url
+    }
+
+def format_response(status, article_count, article_list):
+    """
+    Format a base structure for API response
+
+    :param status: status message
+    :param article_count: total count of returned articles
+    :param article_list: list of returned articles
+    :return: Dictionary format for API response
+    """
+
+    #Default page size from newsapi
+    page_size = 100
+    #Default current_page from newsapi for developer plan
+    current_page = 1
+
+    total_pages = (article_count + page_size - 1) // page_size
+    has_next_page = current_page < total_pages
+
+    response = {
+            "status" : status,
+            "article_count" : article_count,
+            "article_list" : article_list,
+            "pagination": {
+                "current_page" : current_page,
+                "total_pages" : total_pages,
+                "has_next_page" : has_next_page
+            }
+    }
+
+    return response
